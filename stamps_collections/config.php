c331628b328223f2c2701d2e00be8bf5b47af2ee
luca <lucavhw@gmail.com>
2019-04-24 11:51:37 +0200

    updated

--- stamps_collections/config.php
+++ stamps_collections/config.php
@@ -4,7 +4,7 @@
 try
 {
 	// We connect to MySQL
-	$dbConn = new PDO ('mysql:host=localhost;dbname=stamps_app;charset=utf8', 'root', 'Noddels5565');
+	$dbConn = new PDO ('mysql:host=localhost;dbname=stamp_collections;charset=utf8', 'root', 'Noddels5565');
 	echo '***  The connection is succesfully made' . '<br>';
 
 	// set the PDO error mode to exception
